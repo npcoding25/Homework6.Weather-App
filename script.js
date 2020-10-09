70e9057c767e07eafd9e7e1dbdbc61340dcd41ab
Nathan Polomark <polomarkn@hotmail.com>
2020-10-08 20:42:26 -0700

    Fixed date, hide info boxes before searching city,style

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let city = ""
+function showInfo() {
+    let currentWeather = document.querySelector("#current-weather-box")
+    let futureWeather = document.querySelector("#future-weather-cards")
+    currentWeather.classList.remove("hide")
+    futureWeather.classList.remove("hide")
+}
 
+let city = ""
 
 document.querySelector("#searchBtn").addEventListener("click", function(event) {
     event.preventDefault()
@@ -7,6 +13,7 @@ document.querySelector("#searchBtn").addEventListener("click", function(event) {
     console.log(city)
     getWeather()
 })
+
 
 const ApiKey = "9fcd3a5881a397cda2d72d306ef443fe"
 function getWeather() {
@@ -18,16 +25,21 @@ function getWeather() {
     })
     .then(function(response){
         console.log(response)
-        $("#title").text(city)
+        let unixTime = response.dt
+        let date = new Date(unixTime*1000)
+        let year = date.getFullYear()
+        let month = date.getMonth() + 1
+        let day = date.getDate()
+        let showDate = month + "/" + day + "/" + year
+        $("#currentDate").text(showDate)
+        $("#title").text(response.name)
         $("#temp").text("Temperature: " + Math.round(response.main.temp) + "°C")
         $("#humidity").text("Humidity: " + response.main.humidity + "%")
         $("#wind-speed").text("Wind Speed: " + response.wind.speed + " km/h")
         $("#currentIcon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
-        
-        console.log(response.weather[0].icon)
         let latitude = response.coord.lat
         let longitude = response.coord.lon
-          
+        showInfo() 
         function getMoreWeather() {
             let queryUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric" + "&appid=" + ApiKey
 
@@ -40,8 +52,14 @@ function getWeather() {
                 $("#uv-index").text("UV Index: " + response.current.uvi)
                 for ( var i=1; i<=5; i++) {
                     let future = response.daily[i]
-                    $("#date"+ i).text(future.dt)
-                    $("#img" + i).attr("src", "http://openweathermap.org/img/wn/" + future.weather[i].icon + "@2x.png")
+                    let unixTime = future.dt
+                    let date = new Date(unixTime*1000)
+                    let year = date.getFullYear()
+                    let month = date.getMonth() + 1
+                    let day = date.getDate()
+                    let shownDate = month + "/" + day + "/" + year
+                    $("#date"+ i).text(shownDate)
+                    $("#img" + i).attr("src", "http://openweathermap.org/img/wn/" + future.weather[0].icon + "@2x.png")
                     $("#temp-day"+ i).text("Temp: " + Math.round(future.temp.max) + "°C")
                     $("#humidity-day"+ i).text("Humidity: " + future.humidity + "%")
                 }
